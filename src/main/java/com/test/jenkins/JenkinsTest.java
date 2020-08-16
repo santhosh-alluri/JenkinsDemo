1c9bd8a7890e18578a5b60de30b45c93ba529cda
sanall <alluri.santhosh@gmail.com>
2020-08-17 02:24:07 +0530

    basic commits

---
+++ src/main/java/com/test/jenkins/JenkinsTest.java
@@ -0,0 +1,10 @@
+package com.test.jenkins;
+
+public class JenkinsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("I am done");
+	}
+
+}
