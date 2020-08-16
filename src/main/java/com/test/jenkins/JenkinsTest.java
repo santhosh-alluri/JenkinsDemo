c3b12b7a41af7fde6e400dd49ae542c4cd36c6e5
sanall <alluri.santhosh@gmail.com>
2020-08-17 03:20:57 +0530

    temp changes

--- src/main/java/com/test/jenkins/JenkinsTest.java
+++ src/main/java/com/test/jenkins/JenkinsTest.java
@@ -4,7 +4,7 @@ public class JenkinsTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-          System.out.println("I am done");
+          System.out.println("fuck it up man");
 	}
 
 }
